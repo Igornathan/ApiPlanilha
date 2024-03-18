8f923d94f63a3ba65a46f0fe7b6f1f9432fd9042
Igor Nathan Da Silva Almeida <81446483+Igornathan@users.noreply.github.com>
2024-03-18 10:58:05 -0300

    metodo GET

--- src/routes.js
+++ src/routes.js
@@ -3,4 +3,6 @@ const router = express.Router();
 
 const PlanilhaController = require('./controllers/PlanilhaController.js')
 
+router.get('/planilha',PlanilhaController.buscarTodos);
+
 module.exports = router;
